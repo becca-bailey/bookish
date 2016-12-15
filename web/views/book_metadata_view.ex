8a4858134675a932582b4d62fccc00905cbb403d
Becca Nelson <beccanelson88@gmail.com>
2016-12-15 14:25:23 -0600

    Fix tags show route

--- web/views/book_metadata_view.ex
+++ web/views/book_metadata_view.ex
@@ -1,13 +1,4 @@
 defmodule Bookish.BookMetadataView do
   use Bookish.Web, :view
   import Bookish.ViewHelpers
-
-  def number_of_copies(book_metadata) do
-    n = length(book_metadata.books)
-    if n == 1 do
-      "1 copy"
-    else
-      "#{n} copies"
-    end
-  end
 end
